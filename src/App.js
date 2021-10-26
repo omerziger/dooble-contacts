ef21ce3b2e0bde4c3c95e995c32918045b292b55
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-10-26 17:01:35 +0300

    Fetch data on load, and connected components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CssBaseline } from '@material-ui/core';
+import Navbar from './components/Navbar';
+import ContactList from './components/ContactList';
+import Footer from './components/Footer';
+import getUsers from './services/RandomUserAPI';
+import ContactsContext from './context/ContactsContext';
 
 export default function App() {
-  return <div></div>;
+  const [contacts, setContacts] = useState({
+    page: 0,
+    list: [],
+    status: 'idle',
+    search: {
+      value: '',
+      gender: undefined,
+    },
+  });
+
+  useEffect(() => {
+    setContacts((prev) => ({ ...prev, status: 'pending' }));
+    getUsers(1).then((results) =>
+      setContacts((prev) => ({
+        ...prev,
+        page: 1,
+        list: results,
+        status: 'resolved',
+      }))
+    );
+  }, []);
+
+  return (
+    <CssBaseline>
+      <ContactsContext.Provider value={{ contacts, setContacts }}>
+        <Navbar />
+        <ContactList />
+        <Footer />
+      </ContactsContext.Provider>
+    </CssBaseline>
+  );
 }
