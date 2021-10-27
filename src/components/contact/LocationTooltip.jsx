f9df2b1562d37a14fecb6c485a0ce69e87497ba3
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-10-27 10:13:35 +0300

    Tooltip now works onClick instead of hover

--- src/components/contact/LocationTooltip.jsx
+++ src/components/contact/LocationTooltip.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import { makeStyles, MenuItem, Tooltip } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  ClickAwayListener,
+  makeStyles,
+  MenuItem,
+  Tooltip,
+} from '@material-ui/core';
 import { IoMdGlobe } from 'react-icons/io';
 import { IoHome, IoLocationSharp } from 'react-icons/io5';
 import { FaCity } from 'react-icons/fa';
@@ -7,36 +12,47 @@ import { BsSignpostSplitFill } from 'react-icons/bs';
 
 export default function LocationTooltip(props) {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handlePinClick = () => setOpen(!open);
+  const handleTooltipClose = () => setOpen(false);
 
   return (
-    <Tooltip
-      arrow
-      placement='left-start'
-      title={
-        <>
-          <MenuItem disableGutters>
-            <IoMdGlobe className={classes.icon} />
-            {props.location.state}, {props.location.country}
-          </MenuItem>
-          <MenuItem disableGutters>
-            <FaCity className={classes.icon} />
-            {props.location.city}
-          </MenuItem>
-          <MenuItem disableGutters>
-            <IoHome className={classes.icon} />
-            {props.location.street.name} {props.location.street.number}
-          </MenuItem>
-          <MenuItem disableGutters>
-            <BsSignpostSplitFill className={classes.icon} />{' '}
-            {props.location.postcode}
-          </MenuItem>
-        </>
-      }
-    >
-      <span className={classes.pin}>
-        <IoLocationSharp />
-      </span>
-    </Tooltip>
+    <ClickAwayListener onClickAway={handleTooltipClose}>
+      <Tooltip
+        open={open}
+        onClose={handleTooltipClose}
+        arrow
+        placement='left-start'
+        disableFocusListener
+        disableHoverListener
+        disableTouchListener
+        title={
+          <>
+            <MenuItem disableGutters>
+              <IoMdGlobe className={classes.icon} />
+              {props.location.state}, {props.location.country}
+            </MenuItem>
+            <MenuItem disableGutters>
+              <FaCity className={classes.icon} />
+              {props.location.city}
+            </MenuItem>
+            <MenuItem disableGutters>
+              <IoHome className={classes.icon} />
+              {props.location.street.name} {props.location.street.number}
+            </MenuItem>
+            <MenuItem disableGutters>
+              <BsSignpostSplitFill className={classes.icon} />{' '}
+              {props.location.postcode}
+            </MenuItem>
+          </>
+        }
+      >
+        <span className={classes.pin} onClick={handlePinClick}>
+          <IoLocationSharp />
+        </span>
+      </Tooltip>
+    </ClickAwayListener>
   );
 }
 
